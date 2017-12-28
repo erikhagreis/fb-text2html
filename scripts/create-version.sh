3d480663c2e8d05dcc4a0ccd10576011413c96c9
Erik Hagreis <erik.hagreis@gmail.com>
2017-12-28 16:43:26 +0100

    chore: add some tooling

---
+++ scripts/create-version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Working dir must be clean
+git diff-index --quiet HEAD --
+if [ $? -ne 0 ]
+  then
+    echo "Working dir must be clean."
+    exit 1
+fi
+
+# Tests must pass
+# ...
+
+# Compile to ES5
+./node_modules/.bin/babel src --out-dir lib
+git add ./lib
+git commit -m "chore(lib): precompile to ES5"
+
+# Create version with Yarn
+yarn version $1 -m "chore(version): create tag v%s"
+
+# Push!
+git push --follow-tags
